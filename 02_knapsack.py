dcb1ffd335b2c529473e21358437a72aff7c0856
Johniel Bocacao <accounts@johniel.nz>
2022-09-29 21:18:26 +1300

    Finish knapsack

--- 02_knapsack.py
+++ 02_knapsack.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from numpy.random import choice
@@ -6,18 +7,19 @@ import random
 from functools import cmp_to_key
 
 # HYPERPARAMETERS
-mu = 100
-p_cross = 1
-p_mutate = 0.25
-n_elite = 2
-epochs = 500
+mu = 500
+M = int(mu*0.25)
+n_elitism = int(mu*0.05)
+epochs = 25
 
 verbose = False
 global candidates, capacity
 
 
 def parse_data_file(fname):
-    print("===", fname, "===")
+    if verbose:
+        print("===", fname, "===")
+
     file = open(fname)
     header = file.readline().split(" ")
     n = int(header[0])
@@ -36,45 +38,29 @@ def parse_data_file(fname):
 
 
 def generate_population(length):
-    population = []
-    for i in range(mu):
-        ind = []
-        for j in range(length):
-            ind.append(random.choice([True, False]))
-        population.append(ind)
-
-    return population
+    return [[random.choice([True, False]) for j in range(length)] for i in range(mu)]
 
 
 def evaluate(individual):
-    alpha = 2
+    alpha = 50
     value = 0
     weight = 0
     for n, item in enumerate(individual):
         if item:
-            tuple = candidates[n]
-            value += tuple[0]
-            weight += tuple[1]
+            candidate = candidates[n]
+            value += candidate[0]
+            weight += candidate[1]
 
     penalty = alpha * max(0, weight - capacity)
     return value - penalty
-
-
-def evaluate_value(individual):
-    value = 0
-    for n, item in enumerate(individual):
-        if item:
-            tuple = candidates[n]
-            value += tuple[0]
-    return value
 
 
 def is_feasible(individual):
     weight = 0
     for n, item in enumerate(individual):
         if item:
-            tuple = candidates[n]
-            weight += tuple[1]
+            candidate = candidates[n]
+            weight += candidate[1]
     return weight <= capacity
 
 
@@ -82,82 +68,60 @@ def sort_population(i1, i2):
     return evaluate(i2) - evaluate(i1)
 
 
-def roulette_probabilities(population):
-    values = [evaluate(i) for i in population]
-    vmin, vmax = min(values)-1, max(values)  # -1 to allow the minimum value to occupy a non-zero slice
-    norm = [(v - vmin) / (vmax - vmin) for v in values]
-    return [x / sum(norm) for x in norm]
+def main(fname):
+    convergence = []
 
+    global candidates, capacity
+    candidates, capacity = parse_data_file(fname)
+    n = len(candidates)
 
-def one_point_crossover(i1, i2):
-    ind_len = len(i1)
-    idx = random.choice(range(1, ind_len-1))
-    c1 = i1[0:idx] + i2[idx:ind_len]
-    c2 = i2[0:idx] + i1[idx:ind_len]
-    return c1, c2
-
-
-def mutate(individual):
-    idx = random.choice(range(len(individual)))
-    individual[idx] = not individual[idx]
-    return individual
-
-
-def main(seed=None):
-    random.seed(seed)
-    np.random.seed(seed)
-    for f in range(1, len(sys.argv)):  # start from 1, skip 0th argument - filename
-        global candidates, capacity
-        candidates, capacity = parse_data_file(sys.argv[f])
-        n = len(candidates)
+    for seed in range(5):
+        random.seed(seed)
+        np.random.seed(seed)
+        if verbose:
+            print("Seed =", seed)
 
         # Randomly initialise a population of individuals (bit string, each bit has
         # 50% probability to be 1, and 50% to be 0)
         population = generate_population(n)
+        population = sorted(population, key=cmp_to_key(sort_population))
 
-        solution = None
-        convergence = []
+        seed_convergence = []
         for epoch in range(epochs):
-            # Fitness evaluation of each individual
+            # Fitness evaluation of each individual, get best M
+            parents = population[0:M]
+            elites = population[0:n_elitism]
+
+            # For each bit, calculate probability
+            probability = [sum([ind[k] for ind in parents]) / M for k in range(n)]
+
+            # Construct individual based on probability
+            population = elites + [[True if random.uniform(0, 1) < p else False for p in probability] for _ in range(mu-n_elitism)]
             population = sorted(population, key=cmp_to_key(sort_population))
-            feasible_population = [ind for ind in population if is_feasible(ind)]
 
-            # Update the best feasible solution
-            candidate_solution = feasible_population[0] if len(feasible_population) > 0 else population[0]
-            # Only if it's better than the current solution
-            if solution is None or evaluate(candidate_solution) > evaluate(solution):
-                solution = candidate_solution
+            # Record stats
+            best = population[0:5]
+            average = np.mean([evaluate(ind) for ind in best])
+            seed_convergence.append(average)
 
-            # Do elitism (copy top individuals to the new generation)
-            children = feasible_population[0:n_elite] if len(feasible_population) >= n_elite else population[0:n_elite]
+        solution = sorted(population, key=cmp_to_key(sort_population))[0]
 
-            # Sample top 5 for average fitness
-            average_fitness = np.mean([evaluate(ind) for ind in population[0:5]])
-            convergence.append(average_fitness)
+        if verbose:
+            print("Solution:", solution)
+            print("Value:", evaluate(solution))
+        convergence.append(seed_convergence)
 
-            # Generate probabilities for roulette wheel selection
-            roulette = roulette_probabilities(population)
-
-            # Repeat until the new population is full:
-            while len(children) < mu:
-                parents = choice(range(len(roulette)), p=roulette, size=2, replace=False)
-                child1, child2 = population[parents[0]], population[parents[1]]
-                if random.random() < p_cross:
-                    child1, child2 = one_point_crossover(child1, child2)
-                if random.random() < p_mutate:
-                    child1 = mutate(child1)
-                if random.random() < p_mutate:
-                    child2 = mutate(child2)
-                children += [child1, child2]
-            population = children
-
-        print("Solution:", solution)
-        print("Value:", evaluate(solution))
-        filename = sys.argv[f].split("/")[-1]
-        pd.DataFrame(convergence).to_csv("out/part1/"+filename+"_"+str(seed)+".csv")
+    return pd.DataFrame(convergence).T
 
 
 if __name__ == '__main__':
-    for i in range(5):
-        print("Seed =", i)
-        main(i)
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    for f in range(1, len(sys.argv)):  # start from 1, skip 0th argument - filename
+        df = main(sys.argv[f])
+        ax[f-1].set_title(sys.argv[f])
+        df.plot(ax=ax[f-1])
+        if df.min().min() < 0:
+            ax[f-1].set_ylim([0, df.max().max()+100])
+    fig.supxlabel("Epoch")
+    fig.supylabel("Fitness (value)")
+    fig.show()
